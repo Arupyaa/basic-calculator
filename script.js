fca63fa7a53617f8b1aa4808715271490d1d9db5
Arupyaa <doudiat1998@gmail.com>
2025-06-02 05:26:37 +0300

    added equal button and DOM selector grabbers

---
+++ script.js
@@ -0,0 +1,6 @@
+const display = document.querySelector("#display");
+const btn_num = document.querySelectorAll(".btn-num");
+const btn_operator = document.querySelectorAll(".btn-operator");
+const btn_erase = document.querySelector("#btn-erase");
+const btn_cancel = document.querySelector("#btn-cancel");
+const btn_equal = document.querySelector("#btn-equal");
