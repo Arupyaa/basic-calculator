1ef63d6aedab2e039e8960b158a07e8c015686c7
Arupyaa <doudiat1998@gmail.com>
2025-06-02 05:38:16 +0300

    added btn-num parser callback

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const btn_operator = document.querySelectorAll(".btn-operator");
 const btn_erase = document.querySelector("#btn-erase");
 const btn_cancel = document.querySelector("#btn-cancel");
 const btn_equal = document.querySelector("#btn-equal");
+
+for (let element of btn_num) {
+    element.addEventListener("click", function (e) { parseNumber(e.target) });
+}
+
+let calculator = {
+    num1: 0,
+    num2: 0,
+    operator: ''
+};
+
+function parseNumber(target) {
+    if (display.textContent == '0')
+        display.textContent = target.textContent;
+    else
+        display.textContent += target.textContent;
+}
