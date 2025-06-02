d96e63f6d3b404c7565f18bb1bb6cb988fd6e7e5
Arupyaa <doudiat1998@gmail.com>
2025-06-02 06:14:22 +0300

    added btn-operator parser callback

--- script.js
+++ script.js
@@ -8,16 +8,45 @@ const btn_equal = document.querySelector("#btn-equal");
 for (let element of btn_num) {
     element.addEventListener("click", function (e) { parseNumber(e.target) });
 }
+for (let element of btn_operator) {
+    element.addEventListener("click", function (e) { parseOperator(e.target) });
+}
 
 let calculator = {
     num1: 0,
     num2: 0,
-    operator: ''
+    operator: '',
+    isSecondNumber: false
 };
 
 function parseNumber(target) {
+    //check if parseNumber was called for the first time after an operator
+    if(calculator.operator !='' && !calculator.isSecondNumber)
+    {
+        display.textContent = '0';
+        calculator.isSecondNumber = true;
+    }
+
     if (display.textContent == '0')
         display.textContent = target.textContent;
     else
         display.textContent += target.textContent;
+
+
+}
+
+function parseOperator(target) {
+    if (calculator.operator === '') {
+        calculator.num1 = Number(display.textContent);
+    }
+    //call operate on the current 2 numbers if parseOperator was called after a second number was entered
+    else if (calculator.isSecondNumber) {
+        calculator.num1 = calculatorOperate();
+    }
+
+    calculator.operator = target.textContent;
+}
+
+function calculatorOperate(){
+
 }
